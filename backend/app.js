69cf312fe99113c1f6e5d7e4d7ab2d625817de63
Priyanka123-cyber <susanpriyankasebastian@gmail.com>
2024-10-30 19:52:33 +0530

    multer

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,34 @@
 const express=require('express');
 const cors=require('cors');
 const multer=require('multer')
+const path = require('path');
 
 const app= new express();
 app.use(cors());
 app.use(express.json());
 
 
-const storage= multer.diskStorage({
-    destination:function(req,file,cb){
-        return cb(null,"./public/Images")
-    }, 
-    filename: function(req,file,cb){
-        return cb(null,`${ Date.now()}_${file.originalname}`);
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/Images'); // Specify folder
+    },
+    filename: function (req, file, cb) {
+      cb(null, `${file.originalname}_${Date.now()+path.extname(file.originalname)}`); // Keep original name with timestamp
     }
-})
+  });
+  const upload = multer({ storage: storage });
+  
 
-const upload=multer({storage});
+//const upload=multer({dest:'public/Images'});//now we have the above function which defines where the destination is. else give the path directly where storage is given
 
 
 
 app.post('/upload',upload.single('file'),(req,res)=>{
+    //upload.single is a multer middleware which happens which uploads the file to the path
     console.log(req.body);
     console.log(req.file);
 
 })
-
-
 
 app.listen(3001,()=>{
     console.log(`Server is listening at port 3001`);
--- frontend/src/component/Multer.jsx
+++ frontend/src/component/Multer.jsx
@@ -1,17 +1,26 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
-
+ 
 const Multer = () => {
     const [file,SetFile]=useState();
+    //This defines a functional component named Multer.
+    //A state variable file is created with an initial value of undefined.
+    // The SetFile function will be used to update the file value.
 
     const upload=()=>{
-
+        // function that handles the file upload process.
         const formdata= new FormData();
+        //creates a FormData object, which is used to send files and other data as form-encoded multipart data.
         formdata.append('file',file)
+        //adds the selected file to the FormData object with the key 'file'.
         axios.post('http://localhost:3001/upload',formdata)
-        .then(res=>{})
-        .catch(error=>{console.log(error)})
+        // sends a POST request to the specified server endpoint (http://localhost:3001/upload) with the formdata containing the file.
+        //path server sends the request for upload
+        .then(res => {alert("Upload Successful");})
+        .catch(error=>{console.log(error);
+          alert("upload failed");
+        });
     }
 
   return (
